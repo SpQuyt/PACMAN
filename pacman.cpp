e657dad3dda973b8a27f7d63aa43a34f6f3cde9d
SpQuyt <spquyt@gmail.com>
2018-05-10 15:36:02 +0700

    .

--- pacman.cpp
+++ pacman.cpp
@@ -37,7 +37,48 @@ struct Ghost{
     char dir;
 };
 
-int checkF (char **map){
+#define num 9
+char map[num][num];
+
+void init_map(){
+	// create wall
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            map[i][j] = '#';
+        }
+    }	
+	
+    // create fooood
+    for (int i = 1; i < 8; i++) {
+        for (int j = 1; j < 8; j++) {
+            map[i][j] = '.';
+        }
+    }
+    
+	// create obstacle
+	map[4][5] = map[3][6] = map[1][7] = map[7][5] = map[6][1] = 'X';
+}
+
+void display(){
+	system ("cls");	
+	for (int i = 0; i < 9; i++){
+		for (int j = 0; j < 9; j++){
+			cout << ' ' << map[i][j]; 
+		}
+		cout << ' ' <<endl;
+	}
+	_sleep(50);
+}
+
+void move(int x, int y, int dx, int dy){
+	map[x][y] = ' ';
+	x = x + dx;
+	y = y + dy;
+	map[x][y] = 'Q';
+	display();
+}
+
+int checkF(){
 	int count;
 	for (int i = 1; i < 8; i++){
 		for (int j = 1; j < 8; j++){
@@ -51,311 +92,19 @@ int checkF (char **map){
 
 
 int main() {
-	srand(time(NULL));
-    // it's time to create map
-    // create the blank playground size 9x9
 	
-    char **map;
-    map = new char *[9];
-    for(int i = 0; i < 9; i++)
-        map[i] = new char[9];
-
-    // create wall
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            map[i][j] = '#';
-        }
-    }
+	init_map();
 	
-	
-	
-    // create fooood
-    for (int i = 1; i < 8; i++) {
-        for (int j = 1; j < 8; j++) {
-            map[i][j] = '.';
-        }
-    }
-    
-	// create obstacle
-	map[4][5] = map[3][6] = map[1][7] = map[7][5] = map[6][1] = 'X';
-    
-    
     Player quoc;
-    Ghost nam;
+    //Ghost nam;
     quoc.pos_x = quoc.pos_y = 1;
-    nam.pos_x = nam.pos_y = 7;
+    //nam.pos_x = nam.pos_y = 7;
     map[quoc.pos_x][quoc.pos_y] = 'Q';
-    map[nam.pos_x][nam.pos_y] = 'N';
+    //map[nam.pos_x][nam.pos_y] = 'N';
     
-    for (int i = 0; i < 9; i++){
-    	for (int j = 0; j < 9; j++){
-    		cout << ' ' << map[i][j]; 
-		}
-	cout << ' ' <<endl;
-	}
-	
+	display();
 	//====================================================PLAYER=============================================================//
-	quoc.dir = 'd';
-	while (checkF(map) != 0 && (quoc.pos_x != nam.pos_x || quoc.pos_y != nam.pos_y)){
-		switch (quoc.dir){
-			case 'w': 												//nut huong len tren
-					if (map[quoc.pos_x - 1][quoc.pos_y] == 'X'){  //truong hop cham vat can
-						if (GetAsyncKeyState(VK_RIGHT)) {
-							quoc.dir = 'd';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_UP)) {
-							quoc.dir = 'w';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_LEFT)) {
-							quoc.dir = 'a';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_DOWN)){
-							quoc.dir = 's';
-							break;
-						}
-					}
-					else if (GetAsyncKeyState(VK_DOWN)) {
-						quoc.dir = 's';
-						break;
-					}
-					else if (GetAsyncKeyState(VK_LEFT)) {
-						quoc.dir = 'a';
-						break;
-					}
-					else if (GetAsyncKeyState(VK_RIGHT)) {
-						quoc.dir = 'd';
-						break;
-					}
-					else if (quoc.pos_x == 1){ // truong hop cham tuong
-						if (GetAsyncKeyState(VK_RIGHT)) {
-							quoc.dir = 'd';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_UP)) {
-							quoc.dir = 'w';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_LEFT)) {
-							quoc.dir = 'a';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_DOWN)){
-							quoc.dir = 's';
-							break;
-						}
-					}
-					else{
-						map[quoc.pos_x][quoc.pos_y] = ' ';
-						quoc.pos_x--;
-						map[quoc.pos_x][quoc.pos_y] = 'Q';		
-						system ("cls");	
-						for (int i = 0; i < 9; i++){
-						    for (int j = 0; j < 9; j++){
-						    	cout << ' ' << map[i][j]; 
-							}
-								cout << ' ' <<endl;
-						}
-						usleep(100000);	usleep(100000); usleep(100000); usleep(100000);
-					}
-				break;
-				
-			case 'a': 													// nut huong ben trai
-					if (map[quoc.pos_x][quoc.pos_y - 1] == 'X'){ // truong hop cham vat can
-						if (GetAsyncKeyState(VK_RIGHT)) {
-							quoc.dir = 'd';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_UP)) {
-							quoc.dir = 'w';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_LEFT)) {
-							quoc.dir = 'a';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_DOWN)){
-							quoc.dir = 's';
-							break;
-						}
-					}
-					else if (GetAsyncKeyState(VK_DOWN)) {
-						quoc.dir = 's';
-						break;
-					}
-					else if (GetAsyncKeyState(VK_UP)) {
-						quoc.dir = 'w';
-						break;
-					}
-					else if (GetAsyncKeyState(VK_RIGHT)) {
-						quoc.dir = 'd';
-						break;
-					}
-					else if (quoc.pos_y == 1){ // truong hop cham tuong
-						if (GetAsyncKeyState(VK_RIGHT)) {
-							quoc.dir = 'd';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_UP)) {
-							quoc.dir = 'w';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_LEFT)) {
-							quoc.dir = 'a';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_DOWN)){
-							quoc.dir = 's';
-							break;
-						}
-					}
-					else{
-						map[quoc.pos_x][quoc.pos_y] = ' ';
-						quoc.pos_y--;
-						map[quoc.pos_x][quoc.pos_y] = 'Q';	
-						system ("cls");	
-						for (int i = 0; i < 9; i++){
-						    for (int j = 0; j < 9; j++){
-						    	cout << ' ' << map[i][j]; 
-							}
-								cout << ' ' <<endl;
-						}
-						usleep(100000);	usleep(100000); usleep(100000); usleep(100000);
-					}
-				break;
-	
-			case 'd': 														// nut huong ben phai
-					if (map[quoc.pos_x][quoc.pos_y + 1] == 'X'){ // truong hop cham vat can
-						if (GetAsyncKeyState(VK_RIGHT)) {
-							quoc.dir = 'd';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_UP)) {
-							quoc.dir = 'w';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_LEFT)) {
-							quoc.dir = 'a';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_DOWN)){
-							quoc.dir = 's';
-							break;
-						}
-					}
-					else if (GetAsyncKeyState(VK_DOWN)) {
-						quoc.dir = 's';
-						break;
-					}
-					else if (GetAsyncKeyState(VK_UP)) {
-						quoc.dir = 'w';
-						break;
-					}
-					else if (GetAsyncKeyState(VK_LEFT)) {
-						quoc.dir = 'a';
-						break;
-					}
-					else if (quoc.pos_y == 7){ // truong hop cham tuong
-						if (GetAsyncKeyState(VK_RIGHT)) {
-							quoc.dir = 'd';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_UP)) {
-							quoc.dir = 'w';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_LEFT)) {
-							quoc.dir = 'a';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_DOWN)){
-							quoc.dir = 's';
-							break;
-						}
-					}
-					else{
-						map[quoc.pos_x][quoc.pos_y] = ' ';
-						quoc.pos_y++;
-						map[quoc.pos_x][quoc.pos_y] = 'Q';	
-						system ("cls");	
-						for (int i = 0; i < 9; i++){
-						    for (int j = 0; j < 9; j++){
-						    	cout << ' ' << map[i][j]; 
-							}
-								cout << ' ' <<endl;
-						}
-						usleep(400000);
-					}
-				break;
-				
-			case 's': 														// nut huong xuong duoi
-					if (map[quoc.pos_x + 1][quoc.pos_y] == 'X'){ // truong hop cham vat can
-						if (GetAsyncKeyState(VK_RIGHT)) {
-							quoc.dir = 'd';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_UP)) {
-							quoc.dir = 'w';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_LEFT)) {
-							quoc.dir = 'a';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_DOWN)){
-							quoc.dir = 's';
-							break;
-						}
-					}
-					else if (GetAsyncKeyState(VK_RIGHT)) {
-						quoc.dir = 'd';
-						break;
-					}
-					else if (GetAsyncKeyState(VK_UP)) {
-						quoc.dir = 'w';
-						break;
-					}
-					else if (GetAsyncKeyState(VK_LEFT)) {
-						quoc.dir = 'a';
-						break;
-					}
-					else if (quoc.pos_x == 7){ 					// truong hop cham tuong
-						if (GetAsyncKeyState(VK_RIGHT)) {
-							quoc.dir = 'd';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_UP)) {
-							quoc.dir = 'w';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_LEFT)) {
-							quoc.dir = 'a';
-							break;
-						}
-						else if (GetAsyncKeyState(VK_DOWN)){
-							quoc.dir = 's';
-							break;
-						}
-					}
-					else{
-						map[quoc.pos_x][quoc.pos_y] = ' ';
-						quoc.pos_x++;
-						map[quoc.pos_x][quoc.pos_y] = 'Q';	
-						system ("cls");	
-						for (int i = 0; i < 9; i++){
-						    for (int j = 0; j < 9; j++){
-						    	cout << ' ' << map[i][j]; 
-							}
-								cout << ' ' <<endl;
-						}
-						usleep(100000);	usleep(100000); usleep(100000); usleep(100000);
-					}
-				break;
-		} // end of quoc
-	
-	//==========================================================GHOST======================================================//
-	
+	while (checkF() != 0){
+		move(quoc.pos_x, quoc.pos_y, 0 , 1);
 	}
 }
